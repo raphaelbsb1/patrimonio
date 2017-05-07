5990e97839c130e44b399c0e9dbd88f0299f7b67
Raphael Santos <raphael.santos@gmail.com>
2017-05-07 16:00:19 -0300

    Configuração do JPA e do DataSource

--- src/main/java/br/com/evolution/patrimonio/Configuracao.java
+++ src/main/java/br/com/evolution/patrimonio/Configuracao.java
@@ -1,7 +1,11 @@
 package br.com.evolution.patrimonio;
 
+import javax.sql.DataSource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.stereotype.Controller;
 
 @SpringBootApplication
@@ -11,6 +15,17 @@ public class Configuracao {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Configuracao.class, args);
+	}
+	
+	
+	@Bean
+	DataSource getDataSource(){
+		DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+		driverManagerDataSource.setDriverClassName("org.h2.Driver");
+		driverManagerDataSource.setUrl("jdbc:h2:mem:");
+		driverManagerDataSource.setUsername("sa");
+		driverManagerDataSource.setPassword("");
+		return driverManagerDataSource;
 	}
 
 }
--- src/main/java/br/com/evolution/patrimonio/usuario/UsuarioController.java
+++ src/main/java/br/com/evolution/patrimonio/usuario/UsuarioController.java
@@ -1,13 +1,24 @@
 package br.com.evolution.patrimonio.usuario;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class UsuarioController {
 
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
 	@RequestMapping("/")
 	String index(){
 		return "index";
 	}
+	
+	@RequestMapping("usuario")
+	String usuario(){
+		return "usuario";
+	}
+	
+	
 }
