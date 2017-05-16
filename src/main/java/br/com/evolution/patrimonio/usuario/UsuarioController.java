ca3f5ca90f3db0305447edbade09033cbd6b693c
Raphael Santos <raphael.santos@tse.jus.br>
2017-05-16 09:45:46 -0300

    Implementação da configuração segurança para uso do actuator

--- src/main/java/br/com/evolution/patrimonio/usuario/UsuarioController.java
+++ src/main/java/br/com/evolution/patrimonio/usuario/UsuarioController.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import br.com.evolution.EntregadorEmailService;
-
 @Controller
 public class UsuarioController {
 
